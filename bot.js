67a3991b21e59aa0994885eddf929f20a59259f3
Crewmasta <41262507+Crewmasta@users.noreply.github.com>
2018-09-05 13:57:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -65,7 +65,7 @@ client.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
 
-  let prefix = botconfig.prefix;
+  let prefix = "-";
   let messageArray = message.content.split (" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
@@ -92,7 +92,7 @@ client.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
 
-  let prefix = botconfig.prefix;
+  let prefix = "-";
   let messageArray = message.content.split (" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
