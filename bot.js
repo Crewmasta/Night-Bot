0af116268ac8253a89381c78cd659d570f36b5f7
Crewmasta <41262507+Crewmasta@users.noreply.github.com>
2018-09-06 01:29:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,7 +28,8 @@ if(message.content === "-clear"){
 if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No , Check your perms dude");
 if(!args[0]) return message.channel.send("-clear <number>");
 message.channel.bulkDelete(args[0]).then(msg => message.delete(5000))
-  message.channel.send(`\`\`\` ${args[0]} : عدد الرسائل الذي تم مسحها \`\`\` `)
+  message.channel.send(`\`\`\` ${args[0]} : عدد الرسائل الذي تم مسحها \`\`\` `) {
+}
 }
 });
 
